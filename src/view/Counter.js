06ed21fd9be2345bea30ee21e034d7ccf543c1eb
Zofia Lecyk <lecyk.zofia@gmail.com>
2018-05-07 00:02:44 +0200

    ladniejszy counter

--- src/view/Counter.js
+++ src/view/Counter.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function toHHMMSS(sec_num) {
+    var hours   = Math.floor(sec_num / 3600);
+    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+    if (hours   < 10) {hours   = "0"+hours;}
+    if (minutes < 10) {minutes = "0"+minutes;}
+    if (seconds < 10) {seconds = "0"+seconds;}
+    return hours+':'+minutes+':'+seconds;
+}
+
 class Counter extends React.Component {
   constructor(props){
     super(props);
@@ -38,11 +49,8 @@ class Counter extends React.Component {
       <div class="hero-body" onClick={this.restartTimer.bind(this)}>
         <div class="container">
           <h1 class="title">
-            {this.props.to - this.state.time}
+            {toHHMMSS(this.props.to - this.state.time)}
           </h1>
-          <h2 class="subtitle">
-            counter from {this.props.from} to {this.props.to}
-          </h2>
         </div>
       </div>
     </section>
